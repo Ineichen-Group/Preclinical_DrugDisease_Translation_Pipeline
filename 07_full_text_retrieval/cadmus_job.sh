76d054329f8b26a495ce351952f24589dccac2ac
Simona Doneva <donevasimona@gmai.com>
2026-04-08 13:46:45 +0200

    update cadmus scripts

--- 07_full_text_retrieval/cadmus_job.sh
+++ 07_full_text_retrieval/cadmus_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=preclin_fulltext_cadmus
-#SBATCH --time=01:30:00                 # Set a time limit for each job
+#SBATCH --time=20:30:00                 # Set a time limit for each job
 #SBATCH --output=cadmus_output_%A.log  # Save stdout with job and task ID
 #SBATCH --error=cadmus_error_%A.log    # Save stderr with job and task ID
 #SBATCH --mem=16G                      # Memory per node
@@ -8,21 +8,18 @@
 # Add edirect directory to PATH
 export PATH=${PATH}:/data/sdonev/cadmus/output/medline/edirect
 
-# Optional: Activate your virtual environment
-#source activate cadmus-env
-
 # Start timer
 start_time=$(date +%s)
 
-echo "Running run_preclinical_cadmus.py..."
-python run_preclinical_cadmus.py
+echo "Running fetch_cadmus_fulltext.py..."
+python fetch_cadmus_fulltext.py
 
 # Check if the script ran successfully
 if [ $? -ne 0 ]; then
-    echo "run_preclinical_cadmus.py failed to execute."
+    echo "fetch_cadmus_fulltext.py failed to execute."
     exit 1
 else
-    echo "run_preclinical_cadmus.py ran successfully."
+    echo "fetch_cadmus_fulltext.py ran successfully."
 fi
 
 # End timer
--- 07_full_text_retrieval/fetch_cadmus_fulltext_uoe.py
+++ 07_full_text_retrieval/fetch_cadmus_fulltext_uoe.py
@@ -6,17 +6,40 @@ import json
 import pandas as pd
 import random 
 
+
+def load_api_keys(filepath):
+    api_keys = {}
+    with open(filepath, "r") as f:
+        for line in f:
+            line = line.strip()
+            
+            # Skip empty lines or comments
+            if not line or line.startswith("#"):
+                continue
+            
+            key, value = line.split("=", 1)
+            api_keys[key.strip()] = value.strip()
+    
+    return api_keys
+
 #pmid_list = pd.read_csv("cadmus/filtered_df_non_empty_sclerosis_10043_PMIDs.csv")['PMID'].astype(str).tolist()
 with open("cadmus/pmids_without_content_or_only_plain_UoZ.txt", "r") as f1:
     pmid_list = [line.strip() for line in f1 if line.strip()]
    
 print(f"Will be fetching {len(pmid_list)} PMIDs.")
 
+keys = load_api_keys("api_keys.txt")
+
+
+ncbi_api_key = keys.get("NCBI_API_KEY")
+wiley_api_key_uoe = keys.get("wiley_api_key_uoe")
+elsevier_api_key_uoe = keys.get("elsevier_api_key_uoe")
+
 bioscraping(pmid_list,
             "donevasimona@gmail.com", #You need to insert your email address here
-            "c73019fe6b2bfac722029994bf58e17a6d08", #You need to insert your NCBI_API_KEY here
-            wiley_api_key = "867c3eec-80d0-413a-ac62-f2b752d84f71", #This is an optional parameter.
+            ncbi_api_key, #You need to insert your NCBI_API_KEY here
+            wiley_api_key = wiley_api_key_uoe, #This is an optional parameter.
             #You can insert your WILEY_API_KEY here
-            elsevier_api_key = "bd6a6874047737b559b59bd223673bae", #This is an optional parameter.
+            elsevier_api_key = elsevier_api_key_uoe, #This is an optional parameter.
             #You can insert your ELSEVIER_API_KEY here
             )
